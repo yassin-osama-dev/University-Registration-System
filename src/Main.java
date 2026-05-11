2476053c72b00dfa045feb94e565642fd3dfb792
madosuliman26 <mohammed.abdelsamad26@gmail.com>
2026-05-11 21:21:44 +0300

    a message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-System.out.println("Hello Weeee");
+System.out.println("Hello WEE");
 }
